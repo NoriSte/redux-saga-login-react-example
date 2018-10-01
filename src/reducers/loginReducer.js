7caf885c0d6260abd3cc79d897919954c68842e0
Stefano Magni <nori.ste.magni@gmail.com>
2018-10-01 17:41:21 +0200

    moved the status type contants

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,11 +1,12 @@
 import actionTypes from '../actions/actionTypes';
+import { LOGGED_IN, LOGGED_OUT, LOGIN_CANCELLED, LOGIN_ERROR } from './../statusTypes';
 import initialState from './initialState';
 
 export default function userRole(state = initialState.login, action) {
   let newState;
   switch (action.type) {
     case actionTypes.LOGIN_SUCCESS:
-      newState = {...state, status: 'logged in'};
+      newState = {...state, status: LOGGED_IN};
       return newState;
     case actionTypes.SAVE_TOKEN:
       newState = {...state, token: action.token};
@@ -16,13 +17,13 @@ export default function userRole(state = initialState.login, action) {
       newState.token = null;
       return newState;
     case actionTypes.LOGOUT:
-      newState = {...state, status: 'logged out'};
+      newState = {...state, status: LOGGED_OUT};
       return newState;
     case actionTypes.LOGIN_ERROR:
-      newState = {...state, status: 'login error'};
+      newState = {...state, status: LOGIN_ERROR};
       return newState;
     case actionTypes.LOGIN_CANCELLED:
-      newState = {...state, status: 'login cancelled'};
+      newState = {...state, status: LOGIN_CANCELLED};
       return newState;
     default:
       return state;
---
+++ src/statusTypes.js
@@ -0,0 +1,4 @@
+export const LOGGED_IN = 'Logged in';
+export const LOGGED_OUT = 'Logged out';
+export const LOGIN_ERROR = 'Login error';
+export const LOGIN_CANCELLED = 'Login cancelled';
